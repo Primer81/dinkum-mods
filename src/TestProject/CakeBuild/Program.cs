7655bf43c73d2d047d5977906bfd4542b72a1c15
glarkham <gwlarkham@gmail.com>
2025-04-12 22:35:11 -0400

    Add all testing files with TestProject - a working build which was constructed from the template

--- src/TestProject/CakeBuild/Program.cs
+++ src/TestProject/CakeBuild/Program.cs
@@ -26,7 +26,7 @@ public static class Program
 
 public class BuildContext : FrostingContext
 {
-    public const string ProjectName = "InvertHotbarScroll";
+    public const string ProjectName = "TestProject";
     public string BuildConfiguration { get; }
     public string Version { get; }
     public string Name { get; }
---
+++ src/TestProject/TestProject/Plugin.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace TestProject;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
+
